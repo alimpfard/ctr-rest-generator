c8bffcba3a469fc27d81762eb59ed580cec04ec0
AnotherTest <ali.mpfard@gmail.com>
2019-08-01 22:08:38 +0430

    add the example output

---
+++ example/out.js
@@ -0,0 +1,79 @@
+var express = require("express");
+var app = express();
+
+const process_countUsers = require("./endpoints/countUsers.js");
+const process_deletePerson = require("./actions/deletePerson.js");
+const action_deletePerson_0 = require("./actions/deletePerson-action0.js");
+const action_deletePerson_1 = require("./actions/deletePerson-action1.js");
+const process_getPeopleFromCity = require("./actions/getPeopleFromCity.js");
+const action_getPeopleFromCity_0 = require("./actions/getPeopleFromCity-action0.js");
+
+function getOrFail(obj, prop) {
+	let oprop = obj[prop];
+	if (oprop !== null) return oprop;
+	throw new Error(`missing argument ${prop}`);
+}
+let City = {
+	'countryCode': Number,
+	'city': String,
+	'cityCode': Number,
+	'country': String,
+};
+let Person = {
+	'city': City,
+	'lastName': String,
+	'firstName': String,
+};
+app.post("/countUsers", (res, req) => {
+	var bodyStr = "";
+	req.on("data",function(chunk) { bodyStr += chunk.toString(); });
+	req.on("end", function() {
+		let res_input = {};
+			let res_output = {
+				'count': Number,
+			};
+
+			res.send(JSON.stringify(process_countUsers(res_input, res_output)));
+
+		} catch(e) { res.send(JSON.stringify({error: e})); }
+	});
+});
+app.post("/deletePerson", (res, req) => {
+	var bodyStr = "";
+	req.on("data",function(chunk) { bodyStr += chunk.toString(); });
+	req.on("end", function() {
+		try {
+			let jsondec = JSON.parse(bodyStr);
+			let res_input = {
+				'personId': getOrFail(jsondec, 'personId'),
+			};
+
+			let res_output = {
+			};
+			if (action_deletePerson_0(res, req, res_output, res_input))
+				if (action_deletePerson_1(res, req, res_output, res_input))
+					res.send(JSON.stringify(process_deletePerson(res_input, res_output)));
+
+		} catch(e) { res.send(JSON.stringify({error: e})); }
+	});
+});
+app.post("/getPeopleFromCity", (res, req) => {
+	var bodyStr = "";
+	req.on("data",function(chunk) { bodyStr += chunk.toString(); });
+	req.on("end", function() {
+		try {
+			let jsondec = JSON.parse(bodyStr);
+			let res_input = {
+				'city': getOrFail(jsondec, 'city'),
+			};
+
+			let res_output = {
+				'person': [Person],
+			};
+
+			if (action_getPeopleFromCity_0(res, req, res_output, res_input))
+				res.send(JSON.stringify(process_getPeopleFromCity(res_input, res_output)));
+
+		} catch(e) { res.send(JSON.stringify({error: e})); }
+	});
+});
